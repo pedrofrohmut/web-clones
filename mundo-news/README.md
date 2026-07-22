5d1a0626ae4c7962fcdbc18c58df552feb7b618d
pedrofrohmut <pedrofrohmut@gmail.com>
2026-07-22 16:55:20 -0300

    Home Page - Side content - Initial

--- mundo-news/README.md
+++ mundo-news/README.md
@@ -1,6 +1,9 @@
 # Mondo News
 
-A news project website
+ A news project website
+
+ * This project is inspired in real projects and serves only for education and
+ portifolio purposes. There is no commercial intentions behind this development.
 
 ## Credits and Acknowledgments
 
--- mundo-news/src/pages/home/HomePage.tsx
+++ mundo-news/src/pages/home/HomePage.tsx
@@ -33,6 +33,27 @@ const featuredBig: Article = {
   author: "Tabitha Ramalho",
 }
 
+const latestPosts = [
+  {
+    topic: "Brasil",
+    title: "Acusado pelos EUA, Brasil tem mecanismo de combate ao trabalho forçado reconhecido internacionalmente",
+    author: "Bruno de Freitas Moura",
+    subtitle: "Especialistas classificam medidas de Washington como ‘protecionistas’, enquanto governo as considera ‘injustas’ e avalia aplicar Lei de Reciprocidade",
+  },
+  {
+    topic: "Brasil",
+    title: "Seminário ‘Economia de Bem-Estar e soberania’ reúne especialistas para discutir o futuro do Brasil",
+    author: "Redação Opera Mundi",
+    subtitle: "Evento terá mediação do jornalista Luís Nassif e contará com convidados como Márcio Pochmann, Fernando de Souza Coelho e Carlos Gadelha, entre outros",
+  },
+  {
+    topic: "Estados Unidos",
+    title: "Estados Unidos preparam novas tarifas sobre importações de 60 países",
+    author: "Victor Farinelli",
+    subtitle: "Governo Trump justificou medida afirmando que seriam nações que ‘sem legislação contra trabalho forçado, ou não a aplicam’; relatório de 2023, no entanto, aponta que país têm 1,1 milhão de trabalhadores forçados, mais que Brasil",
+  },
+]
+
 const HomePage = () => {
   return (
     <div className="container">
@@ -53,21 +74,21 @@ const HomePage = () => {
               <div className="featured-article-wide__topic home-article-topic">{featuredWide.topic}</div>
               <div className="featured-article-wide__title home-article-title">{featuredWide.title}</div>
               <div className="featured-article-wide__author home-article-author">{featuredWide.author}</div>
-              <div className="featured-article-wide__subtitle">{featuredWide.subtitle}</div>
+              <div className="featured-article-wide__subtitle home-article-subtitle">{featuredWide.subtitle}</div>
             </div>
 
             {/* Featured Article Small */}
             <div className="featured-article-small">
-              <div className="featured-article-small__img home-article-img"></div>
+              <div className="featured-article-small__img home-article-img">small article img</div>
               <div className="featured-article-small__topic home-article-topic">{featuredSmall.topic}</div>
               <div className="featured-article-small__title home-article-title">{featuredSmall.title}</div>
               <div className="featured-article-small__author home-article-author">{featuredSmall.author}</div>
-              <div className="featured-article-small__subtitle">{featuredSmall.subtitle}</div>
+              <div className="featured-article-small__subtitle home-article-subtitle">{featuredSmall.subtitle}</div>
             </div>
 
             {/* Featured Article Big */}
             <div className="featured-article-big">
-              <div className="featured-article-big__img home-article-img"></div>
+              <div className="featured-article-big__img home-article-img">big article img</div>
               <div className="featured-article-big__topic home-article-topic">{featuredBig.topic}</div>
               <div className="featured-article-big__title home-article-title">{featuredBig.title}</div>
               <div className="featured-article-big__author home-article-author">{featuredBig.author}</div>
@@ -77,8 +98,39 @@ const HomePage = () => {
 
         </div>
 
-        <div className="home-content__right">
-          <span>RIGHT SPAN</span>
+        <div className="home-content__right home-sidebar">
+
+          {/* Newsletter Form */}
+          <div className="newsletter">
+            <div className="newsletter__title">Newsletter</div>
+            <form className="newsletter__form">
+              <label htmlFor="email" className="newsletter__label">E-mail</label>
+              <input type="email" id="email" className="newsletter__input required-input" />
+              <button className="newsletter__submit">Inscreva-se!</button>
+            </form>
+          </div>
+
+          {/* Featured Sidebar Video */}
+          <div className="sidebar__featured-video">Sidebar video</div>
+
+          {/* Sidebar Ad */}
+          <div className="sidebar__ad">Sidebar AD</div>
+
+          {/* Sidebar Latest Posts */}
+          <div className="sidebar__latest-posts">
+            {latestPosts.map(post => (
+              <div key={post.title} className="latest-post">
+                <div className="latest-post__topic home-article-topic">{post.topic}</div>
+                <div className="latest-post__title home-article-title">{post.title}</div>
+                <div className="latest-post__author home-article-author">{post.author}</div>
+                <div className="latest-post__subtitle home-article-subtitle">{post.subtitle}</div>
+              </div>
+            ))}
+          </div>
+
+          {/* Sidebar Ad Bigger */}
+          <div className="sidebar__ad-bigger">Sidebar AD</div>
+
         </div>
 
       </main>
