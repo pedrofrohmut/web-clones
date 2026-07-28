80ad921ef5ba45325b5c20b0cdf445a3c66bc682
pedrofrohmut <pedrofrohmut@gmail.com>
2026-07-28 14:12:39 -0300

    Using data files with the type of data available

---
+++ mundo-news/src/data/categories.ts
@@ -0,0 +1,33 @@
+export type Category = {
+  name: string
+  title?: string
+  link?: string
+}
+
+export const categories = [
+  {
+    name: "america-latina",
+    link: "category/america-latina",
+    title: "América Latina"
+  },
+  {
+    name: "guerra-eua-israeal-e-ira",
+    link: "category/guerra-eua-israeal-e-ira",
+    title: "Guerra EUA e Israel x Irã"
+  },
+  {
+    name: "israel-e-palestina",
+    link: "category/israel-e-palestina",
+    title: "Isreal x Palestina"
+  },
+  {
+    name: "mundo-entrevistas",
+    link: "category/mundo-entrevistas",
+    title: "Mundo Entrevistas"
+  },
+  {
+    name: "politica-e-economia",
+    link: "category/politica-e-economia",
+    title: "Política e Economia"
+  },
+]
