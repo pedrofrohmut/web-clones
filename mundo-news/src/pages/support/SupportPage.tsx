694a34a73a6287022aa89d2fa2fa728b601d2582
pedrofrohmut <pedrofrohmut@gmail.com>
2026-08-10 18:06:42 -0300

    SupportPage - Pricing Section

--- mundo-news/src/pages/support/SupportPage.tsx
+++ mundo-news/src/pages/support/SupportPage.tsx
@@ -2,11 +2,13 @@ import "/src/pages/support/support-page.css"
 
 const SupportPage = () => {
   return (
-    <div className="support-page">
+    <div className="container support-page">
 
+      {/* Hero */}
       <div className="support-page__hero-wrapper">
         <div className="support-page__hero">
 
+	  {/* Hero Left - Logo */}
           <div className="support-page__hero-left">
             <div className="support-page__hero-logo">
               <div>Clube Mundo News</div>
@@ -14,6 +16,7 @@ const SupportPage = () => {
             </div>
           </div>
 
+	  {/* Hero Right - Content */}
           <div className="support-page__hero-right">
             <div className="support-page__hero-accent">Ipsum provident elit eveniet libero.</div>
             <div className="support-page__hero-accent">Dolor qui voluptatem minus?</div>
@@ -26,6 +29,42 @@ const SupportPage = () => {
 
         </div>
       </div>
+
+      <section className="support-page__pricing">
+
+	<div className="support-page__pricing-title">Aliquet eget sit amet tellus</div>
+	<div className="support-page__pricing-subtitle">
+          Vel quam elementum pulvinar etiam non quam lacus suspendisse faucibus interdum posuere?
+	</div>
+
+	<div className="support-page__pricing-cards">
+	  <div className="support-page__pricing-card">
+	    <div className="support-page__pricing-card-title">Mensal</div>
+	    <div className="support-page__pricing-card-subtitle">Lacinia quis vel eros donec!</div>
+	    <div className="support-page__pricing-card-price">R$ <span>19,99</span></div>
+	    <div className="support-page__pricing-card-button">Apoie com R$ 19,99</div>
+	  </div>
+
+	  <div className="support-page__pricing-card-highlighted">
+	    <div className="support-page__pricing-card-title">Mensal</div>
+	    <div className="support-page__pricing-card-subtitle">Dolor magna eget est lorem ipsum!</div>
+	    <div className="support-page__pricing-card-price">R$ <span>49,99</span></div>
+	    <div className="support-page__pricing-card-button">Apoie com R$ 49,99</div>
+	  </div>
+
+	  <div className="support-page__pricing-card">
+	    <div className="support-page__pricing-card-title">Anual</div>
+	    <div className="support-page__pricing-card-subtitle">In mollis nunc sed id?</div>
+	    <div className="support-page__pricing-card-price">R$ <span>549,99</span></div>
+	    <div className="support-page__pricing-card-button">Apoie com R$ 549,99</div>
+	  </div>
+	</div>
+
+	<button className="support-page__pricing-other-values">
+          Apoie com outros valores <i className="fa-solid fa-arrow-right"></i>
+	</button>
+
+      </section>
 
     </div>
   )
